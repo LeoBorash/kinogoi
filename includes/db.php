509ec9971b15a4bea2615d574a8bc60fcab5cf0a
leoborash <maulenborashov@gmail.com>
2020-03-31 04:25:37 +0600

    db

--- includes/db.php
+++ includes/db.php
@@ -1,2 +1,3 @@
 <?php 
-$conn = new mysqli("localhost","root","","news");
+// $conn = new mysqli("localhost","root","","news");
+$conn = new mysqli("srv-pleskdb16.ps.kz","trend_ewq","MyPassword2","trendsho_1");
